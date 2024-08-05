8babb63e74eb5ca4c6f976564546fd10351999f8
jini <rose7734@naver.com>
2024-08-05 17:17:04 +0900

    Feat: Plan 컴포넌트 나누기 및 틀잡기

---
+++ src/todotravel/component/plan/ScheduleCreate.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const ScheduleCreate = () => {
+  return (
+    <div>
+      <div>일정 추가하기</div>
+    </div>
+  );
+};
+
+export default ScheduleCreate;
