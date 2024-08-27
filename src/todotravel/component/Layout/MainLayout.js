7430db2899dc6ff877fb20d954a2618882b958df
honeyWater <tkd2756@naver.com>
2024-08-27 13:27:08 +0900

    Refactor: 메인 레이아웃의 자식이 PlanList이면 배너를 띄우도록 수정

--- src/todotravel/component/Layout/MainLayout.js
+++ src/todotravel/component/Layout/MainLayout.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Header from "../Fragment/Header";
 import Footer from "../Fragment/Footer";
+import PlanList from "../plan/PlanList";
 
 import styles from "./Layout.module.css";
 
+import main from "../../../image/main.png";
+
 const MainLayout = ({ children }) => {
+  const isPlanList = React.isValidElement(children) && children.type === PlanList;
+
   return (
     <div className={styles.mainPage}>
       <Header />
+      {isPlanList && (
+        <div className={styles.homeImgWrapper}>
+          <img src={main} alt="홈화면" className={styles.homeImg} />
+        </div>
+      )}
       <div className={styles.mainContent}>{children}</div>
       <Footer />
     </div>
