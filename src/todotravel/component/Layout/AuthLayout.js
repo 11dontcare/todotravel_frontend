06fa061ea50fb880834618a255eeda17ce41ccf9
honeyWater <tkd2756@naver.com>
2024-08-28 13:09:24 +0900

    Refactor: AuthLayout 배경에 랜덤 이미지 배정

--- src/todotravel/component/Layout/AuthLayout.js
+++ src/todotravel/component/Layout/AuthLayout.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../Fragment/Footer";
 
 import styles from "./Layout.module.css";
 
+import background1 from "../../../image/background1.png";
+import background2 from "../../../image/background2.png";
+import background3 from "../../../image/background3.png";
+import background4 from "../../../image/background4.png";
+
+// 배경 이미지 URL 배열
+const backgroundImages = [background1, background2, background3, background4];
+
 const AuthLayout = ({ children }) => {
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    // 랜덤하게 배경 이미지 선택
+    const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+    setBackgroundImage(backgroundImages[randomIndex]);
+  }, []);
+
   return (
     <div className={styles.authPage}>
-      <div className={`${styles.mainContent} ${styles.authContent}`}>{children}</div>
-      <Footer />
+      <div
+        className={styles.backgroundImage}
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
+      <div className={styles.contentOverlay}>{children}</div>
+      <div className={styles.copyright}>
+        <p>© {new Date().getFullYear()} todotravel. All rights reserved.</p>
+        <p>Created by Team 11dontcare</p>
+      </div>
     </div>
   );
 };
