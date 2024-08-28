45f7838e4b6440997cd7edc42b545fafe426aede
honeyWater <tkd2756@naver.com>
2024-08-28 14:38:38 +0900

    Refactor: 로고가 AuthLayout에 배치되도록 수정

--- src/todotravel/component/Layout/AuthLayout.js
+++ src/todotravel/component/Layout/AuthLayout.js
@@ -7,12 +7,14 @@ import background1 from "../../../image/background1.png";
 import background2 from "../../../image/background2.png";
 import background3 from "../../../image/background3.png";
 import background4 from "../../../image/background4.png";
+import { useNavigate } from "react-router-dom";
 
 // 배경 이미지 URL 배열
 const backgroundImages = [background1, background2, background3, background4];
 
 const AuthLayout = ({ children }) => {
   const [backgroundImage, setBackgroundImage] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     // 랜덤하게 배경 이미지 선택
@@ -20,12 +22,18 @@ const AuthLayout = ({ children }) => {
     setBackgroundImage(backgroundImages[randomIndex]);
   }, []);
 
+  // 홈으로 이동
+  const handleTitleClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className={styles.authPage}>
       <div
         className={styles.backgroundImage}
         style={{ backgroundImage: `url(${backgroundImage})` }}
       ></div>
+      <h1 onClick={handleTitleClick}>To Do Travel</h1>
       <div className={styles.contentOverlay}>{children}</div>
       <div className={styles.copyright}>
         <p>© {new Date().getFullYear()} todotravel. All rights reserved.</p>
